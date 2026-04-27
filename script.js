8c74082acd559892700581eddbc57c32e24f626a
bullshit <jerrylavender199@gmail.com>
2026-04-28 06:40:36 +0800

    update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-var FILES_DATA = [];
+var FILES_DATA = [
+  {
+    "id": "1777329633923",
+    "name": "WEAPON.DAT",
+    "author": "pinoolavender",
+    "command": "/dat",
+    "link": "https://bicolink.com/aVA4GTMHjQ",
+    "preview": "",
+    "features": [],
+    "downloads": 0
+  }
+];
