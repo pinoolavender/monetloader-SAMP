2339d8f6bbe79d367ee8c36642819e86f90db6d6
bullshit <jerrylavender199@gmail.com>
2026-04-28 13:58:54 +0800

    update script.js

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 var FILES_DATA = [
   {
+    "id": "1777355932881",
+    "name": "SENSI AIM",
+    "author": "pinoolavender",
+    "command": "/sensi",
+    "link": "https://bicolink.com/odW",
+    "preview": "",
+    "features": [
+      "biar aim lu licin njir"
+    ],
+    "downloads": 0
+  },
+  {
     "id": "1777329633923",
     "name": "WEAPON.DAT",
     "author": "pinoolavender",
