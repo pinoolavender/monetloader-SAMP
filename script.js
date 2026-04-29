6a80f932a19224b829810791e0aad5ac9c67b51b
bullshit <jerrylavender199@gmail.com>
2026-04-29 15:48:37 +0800

    update script.js

--- script.js
+++ script.js
@@ -1,27 +1,5 @@
 var FILES_DATA = [
   {
-    "id": "1777448908930",
-    "name": "UNLIMITED STAMINA",
-    "author": "pinoolavender",
-    "command": "/s",
-    "link": "https://bicolink.com/A8rclEy",
-    "preview": "",
-    "features": [
-      "biar ga haus/capek kalo lari"
-    ],
-    "downloads": 0
-  },
-  {
-    "id": "1777448828226",
-    "name": "P",
-    "author": "P",
-    "command": "P",
-    "link": "p",
-    "preview": "",
-    "features": [],
-    "downloads": 0
-  },
-  {
     "id": "1777356145799",
     "name": "HUD COLOR",
     "author": "pinoolavender",
