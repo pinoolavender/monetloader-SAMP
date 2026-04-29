fb3ef0e69b4322555cbc9133336f45a9413e5cbf
bullshit <jerrylavender199@gmail.com>
2026-04-29 15:49:48 +0800

    update script.js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 var FILES_DATA = [
   {
-    "id": "1777356145799",
-    "name": "HUD COLOR",
-    "author": "pinoolavender",
-    "command": "/hud",
-    "link": "https://bicolink.com/3n9v",
+    "id": "1777448828226",
+    "name": "P",
+    "author": "P",
+    "command": "P",
+    "link": "p",
     "preview": "",
     "features": [],
     "downloads": 0
   },
   {
-    "id": "1777329633923",
-    "name": "WEAPON.DAT",
+    "id": "1777356145799",
+    "name": "HUD COLOR",
     "author": "pinoolavender",
-    "command": "/dat",
-    "link": "https://bicolink.com/aVA4GTMHjQ",
+    "command": "/hud",
+    "link": "https://bicolink.com/3n9v",
     "preview": "",
     "features": [],
     "downloads": 0
