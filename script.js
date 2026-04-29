f4f3d173c70f147f2ec9490e28b8f46f481a54da
bullshit <jerrylavender199@gmail.com>
2026-04-29 15:50:56 +0800

    update script.js

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 var FILES_DATA = [
   {
+    "id": "1777448828226",
+    "name": "P",
+    "author": "P",
+    "command": "P",
+    "link": "p",
+    "preview": "",
+    "features": [],
+    "downloads": 0
+  },
+  {
     "id": "1777356145799",
     "name": "HUD COLOR",
     "author": "pinoolavender",
