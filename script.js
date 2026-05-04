e008b066d4478ff6d085d11e1884181a6c0d881b
bullshit <jerrylavender199@gmail.com>
2026-05-04 10:47:02 +0800

    update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-var FILES_DATA = [
-  {
-    "id": "1777861924136",
-    "name": "y",
-    "author": "y",
-    "command": "y",
-    "link": "Y",
-    "preview": "",
-    "features": [],
-    "downloads": 0
-  }
-];
+var FILES_DATA = [];
